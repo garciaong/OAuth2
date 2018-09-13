b71be55d7b0c373c03de2b332a0ed58555e8c35b
00129761 <00129761@LTEIBCE514877.etiqa-my.MBB.DIR>
2018-09-13 16:55:36 +0800

    Initial commit

---
+++ oauth2/src/main/java/com/springboot/web/oauth2/Oauth2Application.java
@@ -0,0 +1,40 @@
+package com.springboot.web.oauth2;
+
+import java.security.Principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+@RestController
+public class Oauth2Application extends WebSecurityConfigurerAdapter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2Application.class, args);
+	}
+
+	@RequestMapping(value = "/message", method = RequestMethod.GET)
+	public String index() {
+		return "Spring Boot 2 is working!";
+	}
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().antMatcher("/**").authorizeRequests()
+		.antMatchers("/", "/login**", "/webjars/**", "/error**").permitAll()
+				.anyRequest().authenticated()
+				.and().logout().logoutSuccessUrl("/").permitAll();
+	}
+}
